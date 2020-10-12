0240edeaceaaaceb01fae3887da8a05c483e25d6
Evan Bowersock <22BoweE@live.elida.k12.oh.us>
2020-10-12 10:20:17 -0400

    Invoke repeating progress

---
+++ Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeReapting("SpawnBad", 0, 0);
+    }
+
+
+    void SpawnBad()
+    {
+        Debug.Log("Invoked");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
