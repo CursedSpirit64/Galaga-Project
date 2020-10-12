1ce487338ee83781f392568cca3d09ea9ae903ff
Evan Bowersock <22BoweE@live.elida.k12.oh.us>
2020-10-12 10:51:53 -0400

    Invoke Repeating query

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -4,18 +4,33 @@ using UnityEngine;
 
 public class SpawnEnemy : MonoBehaviour
 {
+    public int = badX;
+    public int = badY;
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnBad", 0, 0);
+        StartCoroutine(MyCounter(20));
         //1.5 distance between each enemy, if not functional use 1.75
+    }
+
+    IEnumerator MyCounter(int number)
+    {
+        int i = 0;
+        while (i < number)
+        {
+            Debug.Log("badX is " + badX.ToString());
+            Instantiate(enemy, Vector2(badX, badY), Quaternion.identity);
+            badX -= -1.5f;
+            yield return 0;
+
+            i++;
+        }
     }
 
 
     void SpawnBad()
     {
         Debug.Log("Invoked");
-
     }
 
     // Update is called once per frame
