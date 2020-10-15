4f50c269ace1eef5d09f7ebc86fa2ce8109ba915
Evan Bowersock <22BoweE@live.elida.k12.oh.us>
2020-10-15 10:19:43 -0400

    Enemy Behavior

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {
+    public float velocityEnemy;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<Rigidbody2D>().velocity = - *transform.up * velocityEnemy; 
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if(other.tag == "PlayerBullet")
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
     }
 }
