9dbce7e5425a9485c3355edefae828ad0f091b4b
Evan Bowersock <22BoweE@live.elida.k12.oh.us>
2020-10-15 10:41:54 -0400

    Enemy Spawner adjustments

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    bool isSpawning = false;
+    public float MinSpawnTime = 5f;
+    public float MaxSpawnTime = 5f;
+    public GameObject[] enemies;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    IEnumerator SpawnObject(int index, float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        Instantiate(enemies(index), transform.position, transform.rotation);
+        isSpawning = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isSpawning)
+        {
+            isSpawning = true;
+            int enemyIndex = Random.Range(0, enemies.Length);
+            StartCoroutine(SpawnObject(enemyIndex, Random.Range(MinSpawnTime, MaxSpawnTime)));
+        }
+    }
+    
+    
+}
